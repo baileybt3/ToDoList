b783f16079efbe2508045d8c1c49f90c8d1ec4a5
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2025-08-23 06:39:22 -0400

    feat: added menu loop

--- ToDoList.Console/Program.cs
+++ ToDoList.Console/Program.cs
@@ -5,18 +5,54 @@
 */
 using System;
 
-class ToDoList
+public class ToDoList
 {
+
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Please select and option: ");
-        Console.WriteLine("1. View ToDo List");
-        Console.WriteLine("2. Edit ToDo List");
-        Console.WriteLine("3. Delete TodoList");
-        Console.WriteLine("4. Help");
-        Console.WriteLine("5. Quit");
+        int choice = 0;
 
-        Console.ReadLine();
+        while (choice != 5)
+        {
+            Console.WriteLine("\nPlease select and option: ");
+            Console.WriteLine("1. View To-Do List");
+            Console.WriteLine("2. Edit To-Do List");
+            Console.WriteLine("3. Delete To-Do List");
+            Console.WriteLine("4. Help");
+            Console.WriteLine("5. Quit");
+
+            string input = Console.ReadLine()!;
+
+            if (int.TryParse(input, out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine(" --- To-Do List ---");
+                        break;
+                    case 2:
+                        Console.WriteLine(" --- Editing To-Do List ---");
+                        break;
+                    case 3:
+                        Console.WriteLine(" --- Deleting To-Do List ---");
+                        break;
+                    case 4:
+                        Console.WriteLine(" --- Help ---");
+                        break;
+                    case 5:
+                        Console.WriteLine(" See ya! ");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please select a valid option");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+        }
+        
     }
-
 }
