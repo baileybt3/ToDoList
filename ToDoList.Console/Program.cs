a86920a8e83999418c551c53b801c4bc3a5c6e60
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2025-08-23 06:00:22 -0400

    Initial commit

---
+++ ToDoList.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿/* ToDoList App
+** This program is a ToDoList organizer.
+** Made By: Brandon Bailey
+** 8/23/2025
+*/
+using System;
+
+class ToDoList
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please select and option: ");
+        Console.WriteLine("1. View ToDo List");
+        Console.WriteLine("2. Edit ToDo List");
+        Console.WriteLine("3. Delete TodoList");
+        Console.WriteLine("4. Help");
+        Console.WriteLine("5. Quit");
+
+        Console.ReadLine();
+    }
+
+}
